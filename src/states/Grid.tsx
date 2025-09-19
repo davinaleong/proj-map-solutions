70fc8a4338a239cd32b78cf2ff9e1b89f87d2520
Davina Leong <leong.shi.yun@gmail.com>
2025-09-19 13:53:51 +0800

    Done with the Image grid modal

--- src/states/Grid.tsx
+++ src/states/Grid.tsx
@@ -1,10 +1,12 @@
-import React from "react"
+import React, { useState } from "react"
 import { images } from "./../lib/images"
 import IconGrid from "./../assets/icons/grid-2x2.svg"
 import IconX from "./../assets/icons/x.svg"
 import Image0001 from "./../assets/images/0001.png"
 
 function Grid() {
+  const [isImageGridModalOpen, setIsImageGridModalOpen] = useState(false)
+
   return (
     <>
       <article className="flow">
@@ -16,14 +18,15 @@ function Grid() {
         </header>
         <section className="grid grid-cols-2 grid-rows-2 gap-2 min-h-screen">
           <button
-            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:bg-neutral-100 text-xl"
+            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:opacity-70 text-xl"
             type="button"
           >
             <p>Some content</p>
           </button>
           <button
-            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:bg-neutral-100 text-xl"
+            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square  text-xl hover:opacity-70"
             type="button"
+            onClick={() => setIsImageGridModalOpen(true)}
           >
             <img
               src={Image0001}
@@ -32,13 +35,13 @@ function Grid() {
             />
           </button>
           <button
-            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:bg-neutral-100 text-xl"
+            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:opacity-70 text-xl"
             type="button"
           >
             <p>Some content</p>
           </button>
           <button
-            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:bg-neutral-100 text-xl"
+            className="cursor-pointer font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:opacity-70 text-xl"
             type="button"
           >
             <p>Some content</p>
@@ -46,25 +49,35 @@ function Grid() {
         </section>
       </article>
 
-      <div className="fixed grid place-items-center bg-black/70 inset-0 p-4">
-        <section className="relative max-w-[60ch] bg-white p-4 rounded-sm shadow-sm">
-          <button className="absolute top-4 right-4" type="button">
-            <img src={IconX} alt="Close Icon" width="30px" height="30px" />
-          </button>
+      {isImageGridModalOpen && (
+        <div className="fixed grid place-items-center bg-black/70 inset-0 p-4">
+          <section className="relative max-w-[60ch] bg-white pt-8 pl-4 pb-4 pr-4 rounded-sm shadow-sm">
+            <button
+              className="cursor-pointer absolute top-2 right-2"
+              type="button"
+              onClick={() => setIsImageGridModalOpen(false)}
+            >
+              <img src={IconX} alt="Close Icon" width="20px" height="20px" />
+            </button>
 
-          <div className="flex flex-wrap gap-4">
-            {images.map(({ id, src, alt }) => (
-              <button
-                key={id}
-                type="button"
-                className="cursor-pointer max-w-[15ch] max-h-[15ch] font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm aspect-square hover:opacity-70"
-              >
-                <img className="w-100 h-100 object-cover" src={src} alt={alt} />
-              </button>
-            ))}
-          </div>
-        </section>
-      </div>
+            <div className="grid grid-cols-3 gap-4 overflow-y-auto">
+              {images.map(({ id, src, alt }) => (
+                <button
+                  key={id}
+                  type="button"
+                  className="cursor-pointer max-w-[15ch] min-h-[15ch] font-bold grid place-items-center rounded-sm bg-text-50 shadow-sm overflow-hidden aspect-square hover:opacity-70"
+                >
+                  <img
+                    className="w-100 h-100 object-cover"
+                    src={src}
+                    alt={alt}
+                  />
+                </button>
+              ))}
+            </div>
+          </section>
+        </div>
+      )}
     </>
   )
 }
